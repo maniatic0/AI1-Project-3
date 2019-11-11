be5ec135294f8ba4bb77f5c26ba1ddac06cacec0
Christian Oliveros <christianol_01@hotmail.com>
2019-11-10 21:58:11 -0400

    Ideas

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
+import copy
 
 
-def loadNon(path : str):
+def loadNon(path: str):
     width = -1
     height = -1
+    maxBlockSize = -1
     rowsRules = []
     columnsRules = []
     with open(path, "r") as f:
@@ -19,25 +21,29 @@ def loadNon(path : str):
         columnsRules = [[] for i in range(width)]
 
         # Read First Type Header (Rows or Columns)
-        line = f.readline() # Type Header
+        line = f.readline()  # Type Header
 
-        if (line.strip() == "rows"):
+        if line.strip() == "rows":
             # Read Rows first
             print("First Row Header: " + line.strip())
             for i in range(height):
                 line = f.readline()
                 lineDiv = line.split(",")
                 for rule in lineDiv:
-                    rowsRules[i].append(int(rule))
+                    blockSize = int(rule)
+                    maxBlockSize = max(maxBlockSize, blockSize)
+                    rowsRules[i].append(blockSize)
 
             # Read Columns
-            line = f.readline() # Column Header
+            line = f.readline()  # Column Header
             print("Second Column Header: " + line.strip())
             for i in range(width):
                 line = f.readline()
                 lineDiv = line.split(",")
                 for rule in lineDiv:
-                    columnsRules[i].append(int(rule))
+                    blockSize = int(rule)
+                    maxBlockSize = max(maxBlockSize, blockSize)
+                    columnsRules[i].append(blockSize)
         else:
             # Read Columns first
             print("First Column Header: " + line.strip())
@@ -45,27 +51,105 @@ def loadNon(path : str):
                 line = f.readline()
                 lineDiv = line.split(",")
                 for rule in lineDiv:
-                    columnsRules[i].append(int(rule))
+                    blockSize = int(rule)
+                    maxBlockSize = max(maxBlockSize, blockSize)
+                    columnsRules[i].append(blockSize)
 
             # Read Rows
-            line = f.readline() # Rows Header
+            line = f.readline()  # Rows Header
             print("Second Row Header: " + line.strip())
             for i in range(height):
                 line = f.readline()
                 lineDiv = line.split(",")
                 for rule in lineDiv:
-                    rowsRules[i].append(int(rule))
+                    blockSize = int(rule)
+                    maxBlockSize = max(maxBlockSize, blockSize)
+                    rowsRules[i].append(blockSize)
 
         f.close()
-    return width, height, rowsRules, columnsRules
+    return width, height, maxBlockSize, rowsRules, columnsRules
 
 
-def generate2Dto1DTransform(width, height):
-    return (lambda y, x: x + y * width), 1, width
+def generate2Dto1DTransform(width: int, height: int):
+    return lambda y, x: x + y * width
+
+
+def generate2DAndBlockTo1D(to1D, width: int, height: int, maxBlockSize: int):
+    area = width * height
+    return (
+        lambda k, blockPos, y, x: to1D(y, x) + blockPos * area + k * area * maxBlockSize
+    )
+
+
+def exactlyOne(varList: list, getNewVariable) -> list:
+    clauses = [copy.deepcopy(varList)]  # At least once
+    # for now AMOS
+    listSize = len(varList)
+    for i in range(listSize):
+        for j in range(listSize):
+            clauses.append([-varList[i], -varList[j]])
+    return clauses
+
+
+def generateAllExactlyOneForBlock(
+    block: int,
+    y: int,
+    x: int,
+    blockSize: int,
+    width: int,
+    height: int,
+    doRowWise: bool,
+    getFinalPosition,
+    getNewVariable,
+):
+    clauses = []
+    for blockPos in range(blockSize):
+        if doRowWise:
+            clauses += exactlyOne(
+                [
+                    getFinalPosition(block, blockPos, y, xIter)
+                    for xIter in range(width - (blockSize - blockPos - 1))
+                ],
+                getNewVariable,
+            )
+        else:
+            clauses += exactlyOne(
+                [
+                    getFinalPosition(block, blockPos, yIter, x)
+                    for yIter in range(height - (blockSize - blockPos - 1))
+                ],
+                getNewVariable,
+            )
+    return clauses
+
 
 def main(glucosePath: str, nonPath: str, bmpPath: str):
-    width, height, rowsRules, columnsRules = loadNon(nonPath + ".non")
-    to1D, colStep, rowStep = generate2Dto1DTransform(width, height)
+    width, height, maxBlockSize, rowsRules, columnsRules = loadNon(nonPath + ".non")
+    to1D = generate2Dto1DTransform(width, height)
+    from2DandBlockTo1D = generate2DAndBlockTo1D(to1D, width, height, maxBlockSize)
+
+    variableCount = 0
+    map1DToFinalPos = {}  # We have many empty positions
+    mapFinalPosTo2D = {}
+
+    def getNewVariable():
+        nonlocal variableCount
+        newVar = variableCount
+        variableCount += 1
+        return newVar
+
+    def getFinalPosition(k, blockPos, y, x):
+        pos = from2DandBlockTo1D(k, blockPos, y, x)
+
+        # if we have seen it before
+        if pos in map1DToFinalPos:
+            return map1DToFinalPos[pos]
+
+        # register it
+        finalPos = getNewVariable()
+        map1DToFinalPos[pos] = finalPos
+        mapFinalPosTo2D[finalPos] = (y, x)
+        return finalPos
 
 
 if __name__ == "__main__":
